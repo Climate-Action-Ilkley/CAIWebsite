dd24443fb1e89145ec68956f34f9c4cbfe67bbad
wip-a <wip.abramson@gmail.com>
2019-01-11 21:11:44 +0000

    fix no projects error

--- src/components/ProjectLists/LandProjectFeatures.js
+++ src/components/ProjectLists/LandProjectFeatures.js
@@ -24,9 +24,8 @@ const LandProjectFeatures = () => (
 
     `}
     render={data => {
-      return (
-        <ProjectFeaturesContainer projects={data.allProjectsJson.edges}/>
-      )
+        const toRender = data.allProjectsJson ? <ProjectFeaturesContainer projects={data.allProjectsJson.edges}/>:  <div/>;
+        return toRender;
     }}
   />
 );
