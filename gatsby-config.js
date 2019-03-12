782bf50b0889455611b064bf70bb7cc299cf1834
wip-a <wip.abramson@gmail.com>
2019-03-12 15:15:35 +0000

    remove offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
     'gatsby-plugin-twitter',
     `gatsby-transformer-json`,
     "gatsby-transformer-javascript-frontmatter",
