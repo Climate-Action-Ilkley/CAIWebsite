1fe7f2597943bc3513187b963d395b91f8b23ca5
wip-abramson <will@truu.id>
2019-05-06 10:03:56 +0100

    fixed events in website

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,39 +3,66 @@ import React from 'react'
 import Event from '../components/Event';
 import Layout from '../components/layout'
 
-const Events = ({data}) => {
+const Events = ({ data }) => {
 
-    return (
-      <Layout>
+  const getFutureEvents = (eventEdges) => {
+    const events = filterEvents(eventEdges)
 
-        <div id="main">
-          <section id="content" className="main">
-            {/*<span className="image main"><img src={pic04} alt="" /></span>*/}
-            <h2>Upcoming Events</h2>
-            {data.allEventsJson.edges.map(eventEdge => {
-              return <Event event={eventEdge.node}/>
-            })}
-          </section>
-        </div>
-      </Layout>
-    )
+    if (events.length === 0) {
+      return <h2>No Upcoming Events</h2>
+    } else {
+      return <div>
+        {events.map(event => {
+          return <Event event={event}/>
+        })}
+      </div>
+    }
+  };
+
+  const filterEvents = (eventEdges) => {
+    const events = eventEdges.filter(eventEdge => {
+      const eventDate = new Date(Date.parse(eventEdge.node.date));
+
+      if (eventDate > Date.now()) {
+        return eventEdge.node;
+      }
+    });
+    console.log(events);
+
+    return events;
+  }
+
+
+
+  return (
+    <Layout>
+
+      <div id="main">
+        <section id="content" className="main">
+          {/*<span className="image main"><img src={pic04} alt="" /></span>*/}
+          <h2>Upcoming Events</h2>
+          {getFutureEvents(data.allEventsJson.edges)}
+        </section>
+      </div>
+    </Layout>
+  )
 }
 
 export const eventQuery = graphql`
-  query eventsQuery {
-      allEventsJson(sort: {order: ASC, fields: [date]}) {
-          edges {
-              node {
-                  date
-                  venue
-                  time
-                  name
-                  description
-                  link
-              }
-          }
-      }
-  }
+    query eventsQuery {
+        allEventsJson(sort: {order: ASC, fields: [date]}) {
+            edges {
+                node {
+                    date
+                    venue
+                    time
+                    name
+                    description
+                    link
+                }
+            }
+        }
+    }
 `
 
 export default Events
